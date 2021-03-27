5979ef6b861cbd277334a9b00dfcf75648390a5a
unknown <meir.truong.2309@gmail.com>
2021-03-27 13:50:08 +0700

    update

--- main.js
+++ main.js
@@ -5,138 +5,6 @@ const timer = document.querySelector('.timer')
 const again = document.getElementById('again')
 const start = document.getElementById('start')
 const last_score = document.querySelector('.score')
-function removeElementsByClass(className){
-    var elements = document.getElementsByClassName(className);
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
-}
-
-
-close.addEventListener('click', function(){
-    closeGame();
-})
-function closeGame(){
-    game.style.display = 'none'
-}
-function CountDown(i,count,  callback) {
-    callback = callback || function(){};
-    var int = setInterval(function() {
-        count.innerText = i;
-        i-- || (clearInterval(int), callback());
-    }, 1000);
-
-}
-var score = 0 ;
-
-function startGame(){
-    const count3 = document.querySelector('.count3')
-    count3.style.display = 'block'
-    closeGame();
-    CountDown(2,count3 , function(){
-        count3.style.display = 'none'
-        count3.innerText = '3'
-        board.style.display = 'block'
-        
-        for(let i = 0 ;i< 65; i++)
-            Fall()
-        CountDown(19, timer, function(){
-            board.style.display = 'none';
-            removeElementsByClass('item_fall')
-            const result = document.createElement('div')
-            result.className = 'result mt-3 mx-3 row justify-content-center'
-            if(score < 15){
-                const result_content = document.createElement('div')
-                result_content.className = 'p-3 col-md-6 col-xs-10 position-absolute bg-white text-center'
-                result_content.innerHTML = `
-                            <div class='h2 text-danger'>
-                                RẤT TIẾC CHƯA VƯỢT QUA MINI GAME
-                            </div>
-                            <div class='result_title'>
-                                Bạn đã thất bại mini game với số điểm <h2>`+score+ `/15</h2>  Hãy cố gắng lần sau!
-                            </div>
-                `
-                const again = document.createElement('button')
-
-                again.className = 'btn btn-info'
-                again.id = 'again'
-                again.innerText = 'Chơi lại'
-                again.style.margin = '4%'
-
-                const conti = document.createElement('button')
-                conti.className = 'btn btn-warning'
-                conti.id = 'conti'
-                conti.innerText = 'Tiếp tục'
-                conti.style.margin = '4%'
-
-                // result.appendChild(result_title)
-                // result.appendChild(result_content)
-                result_content.appendChild(again)
-                result_content.appendChild(conti)
-                SoundLose();
-            
-                again.addEventListener('click' ,function(){
-                    score = 0;
-                    last_score.innerText = '0'
-                    timer.innerText = '20'
-                    result.style.display= 'none'
-                    var allcart = document.querySelectorAll('.cart_image')
-                    allcart.forEach(e => {
-                        e.remove()
-                    });
-                    startGame();
-                })
-                result.appendChild(result_content)
-
-            }else{
-
-                const result_content = document.createElement('div')
-                result_content.className ='mx-3 p-3 col-md-6 col-xs-10 position-absolute bg-white text-center'
-                result_content.innerHTML = `
-
-                    <div class='h1 text-danger'>CHÚC MỪNG</div>
-                    <div class='result_header'>
-                    Bạn đã chiến thắng mini game với số điểm <h2>`+score+`/15</h2>  Phần thưởng cho bạn là mã giảm giá 20k dành cho đơn hàng trên 100k tại gian hàng chính hảng của Ajinomoto trên Tiki, Shopee, Lazada
-                    </div>
-                    <div class='voucher my-2 mx-5 py-2 bg-danger text-white h2 '>VR1208</div>
-                    <div class='result_footer fw-bold'>Hãy lưu lại và mua sắm nhé!</div>
-
-                `
-                
-                const conti = document.createElement('button')
-                conti.id = 'conti'
-                conti.className = 'btn btn-primary'
-                conti.innerText = 'Tiếp tục tham quan'
-                conti.style.margin = '4%'
-
-                result_content.appendChild(conti)
-                result.appendChild(result_content)
-                SoundWin();
-
-            }
-                document.body.appendChild(result)
-
-        })
-    })
-}
-
-start.addEventListener('click' , function(e){
-    startGame();
-})
-// 
-const path = './resized/'
-const products = []
-for (let i = 0 ; i <= 76 ; i++)
-    products.push(path  + i + '.png')
-// 
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
-//
-
 function SoundWin(){
     const Win = new Audio('./sounds/wow.mp3')
     Win.play();
@@ -149,16 +17,117 @@ function SoundCart(){
     const Cart = new Audio('./sounds/bounce.mp3')
     Cart.play();
 }
-//
+function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
 
-
+close.addEventListener('click', function(){
+    closeGame();
+})
+function closeGame(){
+    game.style.display = 'none'
+}
+function CountDown(i,count,  callback) {
+    callback = callback || function(){};
+    var int = setInterval(function() {
+        count.innerText = i;
+        (i--) || (clearInterval(int), callback());
+    }, 1000);
+}
+var score = 0 ;
+function startGame(){
+    const count3 = document.querySelector('.count3')
+    count3.style.display = 'block'
+    closeGame();
+    CountDown(2,count3 , function(){
+        count3.style.display = 'none'
+        count3.innerText = '3'
+        board.style.display = 'block'
+        for(let i = 0 ;i< 70; i++)
+            Fall()
+        CountDown(19, timer, function(){
+            board.style.display = 'none';
+            removeElementsByClass('item_fall')
+            const result = document.createElement('div')
+            result.className = 'result mt-5 mx-3 row justify-content-center'
+            if(score < 15){
+                const result_content = document.createElement('div')
+                result_content.className = 'result-child p-3 col-md-6 col-xs-10 position-absolute bg-white text-center'
+                result_content.innerHTML = `
+                            <div class='h2 text-danger'>
+                                RẤT TIẾC CHƯA VƯỢT QUA MINI GAME
+                            </div>
+                            <div class='result_title h4'>
+                                Bạn đã thất bại mini game với số điểm <h2>`+score+ `/15</h2>  Hãy cố gắng lần sau!
+                            </div>
+                `
+                const again = document.createElement('button')
+                again.className = 'btn btn-info'
+                again.id = 'again'
+                again.innerText = 'Chơi lại'
+                again.style.margin = '4%'
+                const conti = document.createElement('button')
+                conti.className = 'btn btn-warning'
+                conti.id = 'conti'
+                conti.innerText = 'Tiếp tục'
+                conti.style.margin = '4%'
+                result_content.appendChild(again)
+                result_content.appendChild(conti)
+                SoundLose();
+                again.addEventListener('click' ,function(){
+                    score = 0;
+                    last_score.innerText = '0'
+                    timer.innerText = '20'
+                    result.style.display= 'none'
+                    var allcart = document.querySelectorAll('.cart_image')
+                    allcart.forEach(e => {
+                        e.remove()
+                    });
+                    startGame();
+                })
+                result.appendChild(result_content)
+            }else{
+                const result_content = document.createElement('div')
+                result_content.className ='result-child mx-3 p-3 col-md-6 col-xs-10 position-absolute bg-white text-center'
+                result_content.innerHTML = `
+                    <div class='h1 text-danger'>CHÚC MỪNG</div>
+                    <div class='result_header'>
+                    Bạn đã chiến thắng mini game với số điểm <h2>`+score+`/15</h2>  Phần thưởng cho bạn là mã giảm giá 20k dành cho đơn hàng trên 100k tại gian hàng chính hảng của Ajinomoto trên Tiki, Shopee, Lazada
+                    </div>
+                    <div class='voucher my-2 mx-5 py-2 bg-danger text-white h2 '>VR1208</div>
+                    <div class='result_footer h4 fw-bold'>Hãy lưu lại và mua sắm nhé!</div>
+                `
+                const conti = document.createElement('button')
+                conti.id = 'conti'
+                conti.className = 'btn btn-primary'
+                conti.innerText = 'Tiếp tục tham quan'
+                conti.style.margin = '4%'
+                result_content.appendChild(conti)
+                result.appendChild(result_content)
+                SoundWin();
+            }
+                document.body.appendChild(result)
+        })
+    })
+}
+start.addEventListener('click' , function(e){
+    startGame();
+})
+const path = './resized/'
+const products = []
+for (let i = 0 ; i <= 76 ; i++)
+    products.push(path  + i + '.png')
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 async function Fall(){
-
     var rand_id = Math.floor(Math.random() * 76)
     var rand_left = Math.floor(Math.random() * 50) + 25
     var rand_speed = (Math.random() * 10) + 5   
     var wait = Math.random() * 50000 + 500 
-
     var item = document.createElement('img')
     item.loading = 'lazy'
     item.className = 'item_fall'
@@ -166,9 +135,6 @@ async function Fall(){
     item.style.left = rand_left + '%';
     item.style.transition = 'all 0.5s';
     board.appendChild(item)
-
-
-
     item.style.display = 'none'
     await sleep(wait)
     item.style.display = 'block'
@@ -182,20 +148,14 @@ async function Fall(){
                 clearInterval(auto)
                 item.remove()
             } 
-
         }, 70);
     }
     ItemFallDown( 0, item, rand_speed)
-
-
     var int
-
-
     var mousePosition;
     var offset = [0,0];
     var isDown = false;
     var isMove = false
-   
     item.addEventListener('mousedown', function(e) {
         clearInterval(auto);
         isDown = true;
@@ -204,7 +164,6 @@ async function Fall(){
             item.offsetTop - e.clientY
         ];
     } , false);
-    
     item.addEventListener('mousemove', function(event) {
         if (isDown) {
             item.style.transition = ''
@@ -221,8 +180,6 @@ async function Fall(){
             item.style.transition = 'all 0.5s'  
         }
     },false);
-
-
     board.addEventListener('mouseup', function(e) {
         if(isMove){
             isDown = false;
@@ -235,19 +192,13 @@ async function Fall(){
                         clearInterval(int)
                         item.remove()
                     } 
-    
                 }, 50);
             }
             ItemFallDown( mousePosition.y + offset[1], item, rand_speed)
         }
         isMove = false;
     },false);
-    
-    
-
-
     const allCart = document.getElementById('cart')
-
     function ChongAnh(){
         var t_img = document.createElement('img')
         t_img.src = item.src 
@@ -256,8 +207,6 @@ async function Fall(){
         t_img.style.transform = 'rotate(' + r_img + 'deg)';
         allCart.appendChild(t_img)
     }
-
-
     function drag(e){
         const cart = document.getElementById('cart_img')
         var x1 = cart.getBoundingClientRect().x
@@ -271,7 +220,6 @@ async function Fall(){
             SoundCart();
             score++;
             last_score.innerText = score;
-            
         }
     }
     
@@ -306,8 +254,22 @@ async function Fall(){
     item.addEventListener('touchstart', function(e) {
         clearInterval(auto);
         isDown2 = true
-    } , false);
 
+        ItemFallDown(item.style.top , item, rand_speed)
+
+    } , false);
+    function ItemFallDown(i , item , speed ){
+        int2 = setInterval(  function() {
+            item.style.top = i + 'px';
+            i+=speed;
+            var hei = document.documentElement.clientHeight
+            if(i >= hei - (hei *  1/100)) {
+                clearInterval(int2)
+                item.remove()
+            } 
+
+        }, 50);
+    }
     item.addEventListener('touchmove',function(e){
             console.log('touch move')
             if(isDown2){
@@ -323,6 +285,7 @@ async function Fall(){
                 item.style.top = mousePosition2.y   +'px';
                 
             }
+            isDown2 = false
         })
 
     
@@ -330,7 +293,8 @@ async function Fall(){
 
 
     item.addEventListener('touchend' , function(e){
-        var el = e.changedTouches[0]
+        if(isDown2){
+            var el = e.changedTouches[0]
                 const cart = document.getElementById('cart_img')
                 var x1 = cart.getBoundingClientRect().x
                 var x2 = x1 + cart.getBoundingClientRect().width
@@ -349,21 +313,11 @@ async function Fall(){
             if(isMove2){
                 item.style.transition = 'all 0.5s'
 
-                function ItemFallDown(i , item , speed ){
-                    int2 = setInterval(  function() {
-                        item.style.top = i + 'px';
-                        i+=speed;
-                        var hei = document.documentElement.clientHeight
-                        if(i >= hei - (hei *  1/100)) {
-                            clearInterval(int2)
-                            item.remove()
-                        } 
-        
-                    }, 50);
-                }
+                
                 ItemFallDown( mousePosition2.y + offset2[1], item, rand_speed)
             }
             isMove2 = false;
+        }
 
     })
 
